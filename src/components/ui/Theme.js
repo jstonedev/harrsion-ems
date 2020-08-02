dcb18249718c5d0e95538c6fa9282c469aa9419b
jstonedev <jstonedev521@gmail.com>
2020-08-02 18:36:49 -0400

    update material theme default colors

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,3 +1,19 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
-export default createMuiTheme({});
+const hemsRed = "#58291f";
+const hemsGreen = "#fbc002";
+
+export default createMuiTheme({
+	palette: {
+		common: {
+			red: `${hemsRed}`,
+			green: `${hemsGreen}`,
+		},
+		primary: {
+			main: `${hemsRed}`,
+		},
+		secondary: {
+			main: `${hemsGreen}`,
+		},
+	},
+});
